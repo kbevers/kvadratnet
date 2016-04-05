26fcad1051ee13b65d0e270f38082521fa8c8586
Kristian Evers <kristianevers@gmail.com>
2016-04-05 21:42:40 +0200

    Created setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='kvadratnet',
+      version='0.1',
+      description='Python tools for working with the danish Kvadratnet tiling scheme.',
+      url='https://github.com/kbevers/kvadratnet',
+      author='Kristian Evers',
+      author_email='kristianevers@gmail.com',
+      license='ISC',
+      py_modules=['kvadratnet'])
