c1590f18aedd420663bdb6a8182032e0f7fad859
Kristian Evers <kristianevers@gmail.com>
2016-04-06 20:05:42 +0200

    Reverted version number because of reasons

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='kvadratnet',
-      version='0.1.1',
+      version='0.1',
       description='Python tools for working with the danish Kvadratnet tiling scheme.',
       url='https://github.com/kbevers/kvadratnet',
       author='Kristian Evers',
       author_email='kristianevers@gmail.com',
-      download_url='https://github.com/kbevers/kvadratnet/tarball/0.1.1',
+      download_url='https://github.com/kbevers/kvadratnet/tarball/0.1',
       license='ISC',
       py_modules=['kvadratnet'])
