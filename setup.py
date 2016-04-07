3d2046821b5c2f496f36c3dd6ae28360d180eb91
Kristian Evers <kristianevers@gmail.com>
2016-04-07 22:44:00 +0200

    Made sure that readme is presented properly on PyPI at next release

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
+"""
+Setup script for the kvadratnet module.
+"""
+
 from setuptools import setup
+
+def readme():
+    """
+    Return a properly formatted readme text, if possible, that can be used
+    as the long description for setuptools.setup.
+    """
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst', format='md')
+    except (IOError, ImportError):
+        with open('README.md') as f:
+            return f.read()
 
 setup(name='kvadratnet',
       version='0.1',
-      description='Python tools for working with the danish Kvadratnet tiling scheme.',
+      description='Python tools for working with the Danish Kvadratnet tiling scheme.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: ISC License (ISCL)',
+          'Topic :: Scientific/Engineering :: GIS',
+          'Topic :: Utilities'
+      ],
+      keywords='kvadratnet gis tiling',
       url='https://github.com/kbevers/kvadratnet',
       author='Kristian Evers',
       author_email='kristianevers@gmail.com',
