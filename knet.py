293320b3c538eb94c9e3858889766a51f15edb3b
Kristian Evers <kristianevers@gmail.com>
2020-03-22 11:30:01 +0100

    Remove outcommented code

--- knet.py
+++ knet.py
@@ -7,23 +7,8 @@ import sys
 import shutil
 import glob
 import argparse
-import fileinput
 from collections import Counter
 import kvadratnet
-
-
-def run_create(args):
-    """
-    Create kvadratnet CLI option.
-    """
-    print("knet create")
-
-
-def run_tindex(args):
-    """
-    Create tile index.
-    """
-    print("knet tindex")
 
 
 def run_rename(args):
@@ -112,7 +97,7 @@ def run_parents(args):
         counter[parent] += 1
 
     if args.unique:
-        for key, value in counter.iteritems():
+        for key, value in counter.items():
             if args.count:
                 print("{:<20} {}".format(key, value))
             else:
@@ -131,29 +116,6 @@ def main():
     """
     parser = argparse.ArgumentParser(prog="knet", description="CLI for kvadratnet")
     subparsers = parser.add_subparsers()
-
-    # create = subparsers.add_parser(
-    #    'create',
-    #   help="""Create a OGR-writable kvadratnet tile grid that covers
-    #           'either a bounding box or a vector-layer""",
-    # )
-    # create.add_argument(
-    #    'area',
-    #    help="""Area that defines the extent of the created kvadratnet
-    #         tile grid. Can be either a path to a vector geometry file
-    #         (shp etc., anything that can be read by OGR) or a bounding
-    #         box on the form [uly ulx lly llx]""",
-    # )
-    # create.add_argument(
-    #    'out',
-    #    help='Output file that stores tile grid. Can be any OGR-writable file type.',
-    # )
-    # create.set_defaults(func=run_create)
-
-    # tindex = subparsers.add_parser('tindex', help='Create a tile index')
-    # tindex.add_argument('path', help='Path to index files')
-    # tindex.add_argument('--exclude', help='Exclude files from path')
-    # tindex.set_defaults(func=run_tindex)
 
     rename = subparsers.add_parser(
         "rename",
